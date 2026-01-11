afde151c500157777212842ca960fd0c6e7dbb44
Zanehisako <yassinetakiko@gmail.com>
2026-01-11 12:46:46 +0100

    Add prediction result handling and fetch functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import { useState } from 'react'
 import './App.css'
 
+interface PredictionResult {
+  message: string;
+  probabilities: number[];
+  prediction?: string;
+}
+
+async function fetchData(home_team: string, away_team: string) {
+  try {
+    const response = await fetch(`http://127.0.0.1:8000/predict/${home_team}/${away_team}?Odds_1=2.00&Odds_X=3.00&Odds_2=2.00`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json(); // or .text(), .blob()
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+
+}
+
 function App() {
   const [homeTeam, sethomeTeam] = useState("")
   const [awayTeam, setawayTeam] = useState("")
+  const [result, setRestult] = useState<PredictionResult | null>(null)
 
   return (
     <>
@@ -30,6 +54,27 @@ function App() {
           </label>
         </div>
         <h2>Matchup: {homeTeam} vs {awayTeam}</h2>
+        <button onClick={async () => {
+          const result = await fetchData(homeTeam, awayTeam)
+          setRestult(result)
+        }}>Submit</button>
+        <div id="result">
+          <div>
+            {result?.message}
+          </div>
+          <div>
+            Home win Proba: {result?.probabilities[2]}
+          </div>
+          <div>
+            Draw Proba: {result?.probabilities[1]}
+          </div>
+          <div>
+            Away win Proba: {result?.probabilities[0]}
+          </div>
+        </div>
+        <div>
+          Winner: {result?.prediction}
+        </div>
       </div>
     </>
   )
