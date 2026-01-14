3389794a52dd22a5aeb3ba7b299b18bb7425b661
Zanehisako <yassinetakiko@gmail.com>
2026-01-14 11:13:45 +0100

    team fuzzy search is working now

--- src/componets/team_search.tsx
+++ src/componets/team_search.tsx
@@ -1,18 +1,62 @@
 import { useState } from "react"
+import Fuse from "fuse.js"
+interface searchResult {
+    item: string,
+    refIndex: number,
+    score: number
+}
 interface TeamSearchProps {
     type?: string,
 }
 export default function TeamSearch(props: TeamSearchProps) {
     const [team, setTeam] = useState<string>("")
+    const [results, setResults] = useState<searchResult[]>([])
+    const options = {
+        includeScore: true
+    }
+
+    const list = [
+        "Arsenal",
+        "Aston Villa",
+        "Bournemouth",
+        "Brentford",
+        "Brighton",
+        "Chelsea",
+        "Crystal Palace",
+        "Everton",
+        "Fulham",
+        "Leeds United",]
+    const fuse = new Fuse(list, options)
+
     return (
         <div>
             <h2>{props.type} team:</h2>
             <input
                 type="text"
                 value={team}
-                onChange={(e) => setTeam(e.target.value)}
+                onChange={(e) => {
+                    setTeam(e.target.value)
+                    const results = fuse.search(e.target.value) as searchResult[]
+                    console.log(results)
+                    setResults(results)
+                }
+                }
                 placeholder={`search for a ${props.type} team ....`}
             />
+            {
+                results.length > 0 && <ul>
+                    {
+                        results.map((result, index) => (
+                            <li key={index} onClick={() => {
+                                setTeam(result.item)
+                                setResults([])
+                            }}>
+                                {result.item}
+                            </li>
+                        ))
+                    }
+                </ul>
+            }
         </div>
     )
 }
